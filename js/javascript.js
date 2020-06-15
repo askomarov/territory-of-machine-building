e222c995057226d7695e388eb583a0b796cf665d
askomarov <63908568+askomarov@users.noreply.github.com>
2020-06-15 13:48:06 +0300

    adaptive mode true

--- js/javascript.js
+++ js/javascript.js
@@ -3,3 +3,10 @@ $(function () {
 		$(this).toggleClass('like-active');
 	});
 });
+
+
+$(document).ready(function () {
+   $('.navbar-toggler').click(function (event) {
+      $('body').toggleClass('lock');
+   })
+});
